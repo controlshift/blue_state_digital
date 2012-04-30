2c285240135720eccb9a4aed1c9b087d5acf6a12
Nathan Woodhull <woodhull@gmail.com>
2012-04-30 16:34:22 +1000

    refactor to use the xml builder.

--- lib/blue_state_digital/constituent_data_builder.rb
+++ lib/blue_state_digital/constituent_data_builder.rb
@@ -1,40 +1,44 @@
+require 'builder'
+
 module BlueStateDigital
   class ConstituentDataBuilder
+
     def self.from_hash(data)
-      xml_data = %q{<?xml version="1.0" encoding="utf-8"?>}
-      xml_data << "<api>"
-      xml_data << cons(data)
-      xml_data << "</api>"
-      xml_data
+      buffer = ""
+      xml = Builder::XmlMarkup.new(:target => buffer)
+      
+      xml.instruct! :xml, :version => "1.0", :encoding => "utf-8"
+      xml.api do | xml |
+        xml = build_constituent(data, xml)
+      end
+      
+      buffer
     end
     
-    def self.cons(data)
-      id_attr = data[:id].nil? ? '' : " id=\"#{data[:id]}\""
-      ext_id_attr = data[:ext_id].nil? ? '' : " ext_id=\"#{data[:ext_id]}\""
-      ext_type_attr = data[:ext_type].nil? ? '' : " ext_type=\"#{data[:ext_type]}\""
-      cons_data = "<cons#{id_attr}#{ext_id_attr}#{ext_type_attr}>"
-      cons_data << "<firstname>#{value_or_default(data[:first_name], '')}</firstname>"
-      cons_data << "<lastname>#{value_or_default(data[:last_name], '')}</lastname>"
-      cons_data << "<is_banned>#{value_or_default(data[:is_banned], '0')}</is_banned>"
-      cons_data << "<create_dt>#{value_or_default(data[:created_at], '')}</create_dt>"
-      emails = data[:emails]
-      unless emails.nil?
-        emails.each do |email|
-          cons_data << cons_email(email)
+    def self.build_constituent(data, xml)
+
+      xml.cons( {:id => data[:id], :ext_id => data[:ext_id], :ext_type => data[:ext_type]}.reject{|k,v| v.nil?} ) do | cons |
+        cons.firstname( value_or_default(data[:first_name], ''))
+        cons.lastname( value_or_default(data[:last_name], ''))
+        cons.is_banned( value_or_default(data[:is_banned], '0'))
+        cons.create_dt( value_or_default(data[:created_at], ''))
+        emails = data[:emails]
+        unless emails.nil?
+          emails.each do |email|
+            email = build_constituent_email(email, cons)
+          end
         end
       end
-      cons_data << "</cons>"
-      cons_data
     end
     
-    def self.cons_email(data)
-      cons_email = '<cons_email>'
-      cons_email << "<email>#{value_or_default(data[:email], '')}</email>"
-      cons_email << "<email_type>#{value_or_default(data[:email_type], 'personal')}</email_type>"
-      cons_email << "<is_subscribed>#{value_or_default(data[:is_subscribed], '0')}</is_subscribed>"
-      cons_email << "<is_primary>#{value_or_default(data[:is_primary], '0')}</is_primary>"
-      cons_email << '</cons_email>'
-      cons_email
+    def self.build_constituent_email(data, cons)
+      cons.cons_email do | cons_email |
+        cons_email.email(value_or_default(data[:email], ''))
+        cons_email.email_type(value_or_default(data[:email_type], 'personal'))
+        cons_email.is_subscribed( value_or_default(data[:is_subscribed], '0'))
+        cons_email.is_primary(value_or_default(data[:is_primary], '0'))
+      end
+      cons
     end
     
     def self.value_or_default(value, default_value)
