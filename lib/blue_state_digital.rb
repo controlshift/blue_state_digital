e73ba8172fa44cb5cb81a99c7fad9d6cd7978570
Nathan Woodhull <woodhull@gmail.com>
2012-10-19 16:52:16 -0400

    remove duplication.

--- lib/blue_state_digital.rb
+++ lib/blue_state_digital.rb
@@ -4,10 +4,6 @@ require 'builder'
 require 'nokogiri'
 require 'active_support/core_ext'
 require 'crack/xml'
-require 'builder'
-require 'nokogiri'
-require 'active_support/core_ext'
-require 'crack/xml'
 
 require "blue_state_digital/version" unless defined?(BlueStateDigital::VERSION)
 require "blue_state_digital/connection"
