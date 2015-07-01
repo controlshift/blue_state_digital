d8a038d718a62a0d347175c2a740f7e831816a53
Nathan Woodhull <woodhull@gmail.com>
2015-07-01 16:26:58 -0400

    more typed exceptions from BSD middleware gem.

--- lib/blue_state_digital/error_middleware.rb
+++ lib/blue_state_digital/error_middleware.rb
@@ -1,5 +1,6 @@
 module BlueStateDigital
   class Unauthorized < ::Faraday::Error::ClientError ; end
+  class ResourceDoesNotExist < ::Faraday::Error::ClientError ; end
   class ErrorMiddleware < ::Faraday::Response::RaiseError
     def on_complete(env)
       case env[:status]
@@ -7,6 +8,12 @@ module BlueStateDigital
         raise Faraday::Error::ResourceNotFound, response_values(env).to_s
       when 403
         raise BlueStateDigital::Unauthorized, response_values(env).to_s
+      when 409
+        if env.body =~ /does not exist/
+          raise BlueStateDigital::ResourceDoesNotExist, response_values(env).to_s
+        else
+          raise Faraday::Error::ClientError, response_values(env).to_s
+        end
       when 407
         # mimic the behavior that we get with proxy requests with HTTPS
         raise Faraday::Error::ConnectionFailed, %{407 "Proxy Authentication Required "}
--- spec/blue_state_digital/error_middleware_spec.rb
+++ spec/blue_state_digital/error_middleware_spec.rb
@@ -6,4 +6,10 @@ describe BlueStateDigital::ErrorMiddleware do
       subject.on_complete(OpenStruct.new({status: 409, body: 'foo bar'}))
     end.to raise_error(Faraday::Error::ClientError, /foo bar/)
   end
+
+  it 'should raise a typed exception' do
+    expect do
+      subject.on_complete(OpenStruct.new({status: 409, body: 'cons_group_id #279 does not exist'}))
+    end.to raise_error(BlueStateDigital::ResourceDoesNotExist, /cons_group_id #279 does not exist/)
+  end
 end
