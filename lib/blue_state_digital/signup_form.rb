d7646b93ab845c1a498d4e3fca106ab9382cd096
Jacinda Moore <alynna.kasmira@gmail.com>
2016-01-20 17:34:43 -0500

    notes on process_signup

--- lib/blue_state_digital/signup_form.rb
+++ lib/blue_state_digital/signup_form.rb
@@ -2,6 +2,7 @@ module BlueStateDigital
   class SignupForm < ApiDataModel
     FIELDS = [:id, :name, :slug, :public_title, :create_dt]
     attr_accessor *FIELDS
+    attr_accessor :form_fields
 
     def self.clone(options)
       clone_from_id = options[:clone_from_id]
@@ -22,5 +23,20 @@ module BlueStateDigital
         raise "Set constituent data failed with message: #{xml_response}"
       end
     end
+
+    def process_signup(data)
+      # TODO: fetch form fields if they don't exist yet
+      # construct XML with the data
+      # call process_signup endpoint
+      # return success or errors
+    end
+
+    private
+
+    def fetch_form_fields
+      # TODO: call list_form_fields and set form_fields with what comes back
+    end
   end
+
+  # TODO: SignupFormField class
 end
